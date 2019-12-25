294103ac9375bc8484215f50b816b5d809f6c6ab
Patrick McClurg <patrick@noumenadigital.com>
2019-12-25 20:59:20 +0100

    Support for bang and minus prefix expressions (page 119)

--- src/main/kotlin/cluelessuk/Runtime.kt
+++ src/main/kotlin/cluelessuk/Runtime.kt
@@ -3,17 +3,42 @@ package cluelessuk
 sealed class MObject
 data class MInteger(val value: Int) : MObject()
 data class MBoolean(val value: Boolean) : MObject()
+
 object Null : MObject()
+
+val True = MBoolean(true)
+val False = MBoolean(false)
 
 
 class MonkeyRuntime {
-    fun eval(node: Node): MObject {
-        return when (node) {
+    fun eval(node: Node): MObject =
+        when (node) {
             is Program -> eval(node.statements.last())
             is ExpressionStatement -> eval(node.expression)
             is IntegerLiteral -> MInteger(node.value)
-            is BooleanLiteral -> MBoolean(node.value)
+            is BooleanLiteral -> if (node.value) True else False
+            is PrefixExpression -> evalPrefixExpression(node.operator, eval(node.right))
             else -> Null
         }
-    }
+
+    private fun evalPrefixExpression(operator: String, right: MObject): MObject =
+        when (operator) {
+            "!" -> evalBangExpression(right)
+            "-" -> evalMinusPrefixExpression(right)
+            else -> Null
+        }
+
+    private fun evalBangExpression(right: MObject): MObject =
+        when (right) {
+            True -> False
+            False -> True
+            is Null -> True
+            else -> False
+        }
+
+    private fun evalMinusPrefixExpression(right: MObject) =
+        when (right) {
+            is MInteger -> MInteger(-right.value)
+            else -> Null
+        }
 }
--- src/test/groovy/cluelessuk/EvalKtTest.groovy
+++ src/test/groovy/cluelessuk/EvalKtTest.groovy
@@ -5,6 +5,8 @@ import spock.lang.Specification
 class EvalKtTest extends Specification {
 
     def evaluator = new MonkeyRuntime()
+    def static True = new MBoolean(true)
+    def static False = new MBoolean(false)
 
     def "Single integer literal evaluates to itself"() {
         given:
@@ -27,6 +29,34 @@ class EvalKtTest extends Specification {
         def result = evaluator.eval(program)
 
         then:
-        result == new MBoolean(true)
+        result == True
+    }
+
+    def "Bang operator negates and coerces to a boolean"(String input, MBoolean expected) {
+        given:
+        def result = evaluator.eval(new Parser(new Lexer(input)).parseProgram())
+
+        expect:
+        result == expected
+
+        where:
+        input     | expected
+        "!true"   | False
+        "!false"  | True
+        "!5"      | False
+        "!!true"  | True
+        "!!false" | False
+        "!!5"     | True
+    }
+
+    def "Negative integer expression evaluates to itself"() {
+        given:
+        def input = "-10"
+
+        when:
+        def result = evaluator.eval(new Parser(new Lexer(input)).parseProgram())
+
+        then:
+        result == new MInteger(-10)
     }
 }
